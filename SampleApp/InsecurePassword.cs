c7a94013f15c056f7e410cb24988ebe3fd48ddb2
Irfan Syed <isyed@progress.com>
2020-12-16 15:15:54 -0500

    Update InsecurePassword.cs
    Adding bad code for CodeQL to identify

--- SampleApp/InsecurePassword.cs
+++ SampleApp/InsecurePassword.cs
@@ -3,5 +3,17 @@ using System;
 
 public class InsecurePassword
 {
+
+	public bool badpassword()
+	{
+		string password = "password";
+
+		if (password == "password")
+		{
+			return true;
+		}
+
+		return false;
+	}
 	
 }
